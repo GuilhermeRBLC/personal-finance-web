4d9f1dbeb62083bcf0c8465f7fcaf2e3266c8235
Guilherme <guilhermerblc@gmail.com>
2026-06-07 14:16:31 -0300

    feat: register layout done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Accounts } from './pages/accounts/accounts';
+import { Transactions } from './pages/transactions/transactions';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full' 
+    },
+    { 
+        path: 'login', 
+        loadComponent: () => Login 
+    },
+    { 
+        path: 'register', 
+        loadComponent: () => Register 
+    },
+
+    // Rotas Internas do Sistema
+    { 
+        path: 'dashboard', 
+        loadComponent: () => Dashboard 
+    },
+    { 
+        path: 'accounts', 
+        loadComponent: () => Accounts 
+    },
+    {
+        path: 'transactions', 
+        loadComponent: () => Transactions 
+    },
+
+    { path: '**', redirectTo: 'login' }
+];
--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {}
+export class Register {
+
+  registerData = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  onSubmit() {
+    console.log('Dados de registro enviados:', this.registerData);
+    // Chamar API
+  }
+
+}
