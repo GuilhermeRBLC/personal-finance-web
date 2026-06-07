1bc8fc0b9aae1568ed5929dbfe4230670395c81d
Guilherme <guilhermerblc@gmail.com>
2026-06-07 19:43:00 -0300

    feat: dashboard API coneccted

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,14 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-interface Transaction {
-  id: number;
-  description: string;
-  amount: number;
-  date: string;
-  type: 'INCOME' | 'EXPENSE';
-  category: string;
-}
+import { DashboardService, DashboardSummary } from '../../services/dashboard/dashboard';
+import { Transaction } from '../../services/transactions/transactions';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,36 +9,39 @@ interface Transaction {
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
 
-  transactions: Transaction[] = [
-    { id: 1, description: 'Salário Mensal', amount: 5000.00, date: '2026-06-01', type: 'INCOME', category: 'Trabalho' },
-    { id: 2, description: 'Supermercado', amount: 450.30, date: '2026-06-02', type: 'EXPENSE', category: 'Alimentação' },
-    { id: 3, description: 'Freelance Web', amount: 1500.00, date: '2026-06-04', type: 'INCOME', category: 'Trabalho' },
-    { id: 4, description: 'Conta de Luz', amount: 180.20, date: '2026-06-05', type: 'EXPENSE', category: 'Contas' },
-    { id: 5, description: 'Gasolina', amount: 120.00, date: '2026-06-06', type: 'EXPENSE', category: 'Transporte' },
-  ];
+  dashboardSummary = signal<DashboardSummary>({ currentBalance: 0, recentTransactions: [], totalExpense: 0, totalIncome: 0 });
+
+  private dashboardService = inject(DashboardService);
+  
+  ngOnInit() {
+    this.loadDashboardSummary();
+  }
+
+  loadDashboardSummary() {
+    this.dashboardService.getSummary().subscribe({
+      next: (data) => {
+        this.dashboardSummary.set(data);
+      },
+      error: (err) => console.error('Erro obter informações do dashboard:', err)
+    });
+  }
 
   get totalIncome(): number {
-    return this.transactions
-      .filter(t => t.type === 'INCOME')
-      .reduce((sum, t) => sum + t.amount, 0);
+    return this.dashboardSummary().totalIncome;
   }
 
   get totalExpense(): number {
-    return this.transactions
-      .filter(t => t.type === 'EXPENSE')
-      .reduce((sum, t) => sum + t.amount, 0);
+    return this.dashboardSummary().totalExpense;
   }
 
   get currentBalance(): number {
-    return this.totalIncome - this.totalExpense;
+    return this.dashboardSummary().currentBalance;
   }
   
   get recentTransactions(): Transaction[] {
-    return this.transactions
-      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
-      .slice(0, 3);
+    return this.dashboardSummary().recentTransactions;
   }
 
 }
---
+++ src/app/services/dashboard/dashboard.ts
@@ -0,0 +1,26 @@
+import { Service } from '@angular/core';
+import { inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Transaction } from '../transactions/transactions';
+
+export interface DashboardSummary {
+  totalIncome: number;
+  totalExpense: number;
+  currentBalance: number;
+  recentTransactions: Transaction[]; // Substitua pelo seu tipo de transação se quiser
+}
+
+@Service()
+export class DashboardService {
+
+    private http = inject(HttpClient);
+
+    private apiUrl = 'http://localhost:8080/api/dashboard';
+
+    getSummary(): Observable<DashboardSummary> {
+        const userId = localStorage.getItem('token');
+        return this.http.get<DashboardSummary>(`${this.apiUrl}/summary/${userId}`);
+    }
+
+}
