e9663325bf7cbe458327732d73d739823135b6a2
Guilherme <guilhermerblc@gmail.com>
2026-06-07 14:28:56 -0300

    feat: login layout

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+
+  loginData = {
+    email: '',
+    password: ''
+  };
+
+  onSubmit() {
+    console.log('Tentativa de login com:', this.loginData);
+    // Chamar API
+  }
+
+}
