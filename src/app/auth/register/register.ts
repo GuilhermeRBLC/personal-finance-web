ed57f59a2fa4a1fea422f51ae99bd97f99b9149f
Guilherme <guilhermerblc@gmail.com>
2026-06-07 15:41:10 -0300

    feat: register api connected

--- src/app/auth/register/register.ts
+++ src/app/auth/register/register.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Auth } from '../../services/auth/auth';
 
 
 @Component({
@@ -17,9 +18,21 @@ export class Register {
     password: ''
   };
 
+  private authService = inject(Auth);
+  private router = inject(Router);
+
   onSubmit() {
-    console.log('Dados de registro enviados:', this.registerData);
-    // Chamar API
+    this.authService.register(this.registerData).subscribe({
+      next: (response) => {
+        console.log('Registro realizado com sucesso!', response);
+        
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.error('Erro ao fazer registro:', err);
+        alert('Falha no registro. Verifique seus dados!');
+      }
+    });
   }
 
 }
